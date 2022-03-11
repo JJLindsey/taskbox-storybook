85aa28e43ba4dad7c682cc9a192961ef74821a15
JJLindsey <jlindsey010@gmail.com>
2022-03-10 16:45:58 -0800

    add task and task child/stories, update Main & Preview with components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import '../src/index.css'
+//configures Storybook to log the actions (onArchive & onPinTask) in the UI
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
